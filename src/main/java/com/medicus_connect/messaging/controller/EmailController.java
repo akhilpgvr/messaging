2dcd2af19107029677df0644e5a1d89b939a48ab
akhilpgvr <akhilpgvr@gmail.com>
2025-02-08 20:26:11 +0530

    updated the reponse format (archotecture) to response entity standard

--- src/main/java/com/medicus_connect/messaging/controller/EmailController.java
+++ src/main/java/com/medicus_connect/messaging/controller/EmailController.java
@@ -3,6 +3,8 @@ package com.medicus_connect.messaging.controller;
 import com.medicus_connect.messaging.model.request.MessageRequest;
 import com.medicus_connect.messaging.service.EmailService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -17,27 +19,27 @@ public class EmailController {
     }
 
     @PostMapping("/sendText")
-    public String sendTextEmail(@RequestBody MessageRequest messageRequest) {
+    public ResponseEntity<String> sendTextEmail(@RequestBody MessageRequest messageRequest) {
 
         log.info("Calling email service");
         emailService.sendTextEmail(messageRequest);
-        return "Text email sent successfully!";
+        return new ResponseEntity<>("Text email sent successfully!", HttpStatus.OK);
     }
 
     @PostMapping("/sendHtml")
-    public String sendHtmlEmail(@RequestParam String to,
+    public ResponseEntity<String> sendHtmlEmail(@RequestParam String to,
                                 @RequestParam String subject,
                                 @RequestParam String htmlContent) {
         emailService.sendHtmlEmail(to, subject, htmlContent);
-        return "HTML email sent successfully!";
+        return new ResponseEntity<>("HTML email sent successfully!", HttpStatus.OK);
     }
 
     @PostMapping("/sendWithAttachment")
-    public String sendEmailWithAttachment(@RequestParam String to,
+    public ResponseEntity<String> sendEmailWithAttachment(@RequestParam String to,
                                           @RequestParam String subject,
                                           @RequestParam String body,
                                           @RequestParam String filePath) {
         emailService.sendEmailWithAttachment(to, subject, body, filePath);
-        return "Email with attachment sent successfully!";
+        return new ResponseEntity<>("Email with attachment sent successfully!", HttpStatus.OK);
     }
 }
